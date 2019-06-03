b761d232d9054e53b159dd18bf5ae52e1483a27e
Teoxoy <28601907+Teoxoy@users.noreply.github.com>
2019-06-04 01:06:19 +0200

    remove polka leftover

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,4 @@ if (dev) {
 
 express()
     .use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
-    .listen(PORT, err => {
-        if (err) {
-            console.log('error', err)
-        }
-    })
+    .listen(PORT)
