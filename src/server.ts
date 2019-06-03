3589d44bf188e5ccc64166a49d6f1f87e6c6eda5
Teoxoy <28601907+Teoxoy@users.noreply.github.com>
2019-06-04 00:00:10 +0200

    add env file config capability

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ import * as sapper from '@sapper/server'
 const { PORT, NODE_ENV } = process.env
 const dev = NODE_ENV === 'development'
 
+if (dev) {
+    require('dotenv').config()
+}
+
 express()
     .use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
     .listen(PORT, err => {
