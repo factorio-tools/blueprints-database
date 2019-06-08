b56ac7340f74cbc7949a67968993b99c72074797
Teoxoy <28601907+Teoxoy@users.noreply.github.com>
2019-06-08 12:35:41 +0200

    make eslint happy

--- src/utils/ui-utils.ts
+++ src/utils/ui-utils.ts
@@ -1,10 +1,8 @@
 import * as sapper from '@sapper/app'
 
 // Curried callback wrapper function for passing to component
-const goto = (path:string) => {
-    return () => {
-        sapper.goto(path)
-    }
+const goto = (path: string) => () => {
+    sapper.goto(path)
 }
 
 export { goto }
