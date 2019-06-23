0a711c1aca7d13130c2157e2123f253e3d99c5f1
Tyler Pearce <tyler@stuffedmotion.ca>
2019-06-22 21:27:43 -0400

    Error component

---
+++ src/components/Layout/Form/Error.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { fly } from 'svelte-transitions'
+    export let message
+</script>
+
+<style lang="scss">
+    div {
+        padding: 10px;
+        margin-bottom: 14px;
+        background: rgba(255, 63, 79, 0.4);
+        border-top: 3px solid rgba(255, 63, 79, 0.4);
+        color: #fdf9f9;
+        font-size: 13px;
+    }
+</style>
+
+<div transition:fly={{ y: 20 }}>{message}</div>
